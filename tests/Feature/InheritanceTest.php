86ed4c689f3e7d24c6e369df64aee2616334b46d
Ivriel <gunawanivriel@gmail.com>
2025-11-27 20:35:59 +0700

    template inheritance

---
+++ tests/Feature/InheritanceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InheritanceTest extends TestCase
+{
+    public function testParent()
+    {
+        $this->view("child",[])
+        ->assertSeeText("Nama Aplikasi - Halaman Utama")
+        ->assertSeeText("Default Header")
+        ->assertSeeText("Deskripsi Header")
+        ->assertDontSeeText("Deskripsi Content")
+        ->assertSeeText("Ini adalah halaman utama")
+        ;
+    }
+
+      public function testInheritanceWithoutOverride()
+    {
+        $this->view("child-default",[])
+        ->assertSeeText("Nama Aplikasi - Halaman Utama")
+        ->assertSeeText("Default Header")
+        ->assertSeeText("Default Content")
+        ->assertDontSeeText("Deskripsi Header")
+        ->assertDontSeeText("Ini adalah halaman utama")
+        ;
+    }
+}
